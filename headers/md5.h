65c0ca8803c21d866ad7c9993ac1b8f5b3e6b661
Cosmao <emil.kool@hotmail.com>
2024-01-18 11:59:04 +0100

    Magic md5 constants

--- headers/md5.h
+++ headers/md5.h
@@ -2,13 +2,15 @@
 #define md5_h
 
 #include "headers/hashing.h"
+#include <cstdint>
 
-class md5 : public hashing{
+class md5 : public hashing {
 private:
+  static const std::uint32_t s[64];
+  static const std::uint32_t k[64];
 
 public:
   std::string hashString(std::string);
 };
-
 
 #endif // !md5_h
