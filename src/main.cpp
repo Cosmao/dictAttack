101a2286888bdad5e4ca6eb1c979e314fae19715
Cosmao <emil.kool@hotmail.com>
2024-01-23 09:54:22 +0100

    Fixed so CMake uses cpp 23 and added std::format

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,17 @@
 #include "headers/openSSL_MD5.h"
 #include "headers/userMenu.h"
 #include <iostream>
+#include <format>
 
 #define filePath "filePath"
-
 
 int main(void) {
   io_hander io(filePath);
   openSSL_MD5 md5Hasher;
   userMenu menu(io, md5Hasher);
   std::string input = "The quick brown fox jumps over the lazy dog";
-  std::string hashed = md5Hasher.hashString(input);
+  std::string hashedInput = md5Hasher.hashString(input);
 
-  std::cout << "Original: " << input << "\n";
-  std::cout << "Hashed: " << hashed << "\n";
-
+  std::cout << std::format("Original: {}\nHashed: {}", input, hashedInput) << std::endl;
   return 0;
 }
