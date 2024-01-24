5f3c3991e242f9e5e1d6c7b3cd33186febde3981
Cosmao <emil.kool@hotmail.com>
2024-01-24 11:58:16 +0100

    Added sha256 support

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "headers/io_file.h"
 #include "headers/openSSL_MD5.h"
+#include "headers/openSSL_SHA256.h"
 #include "headers/userMenu.h"
 #include <iostream>
 #include <format>
@@ -9,10 +10,12 @@
 int main(void) {
   io_hander io(filePath);
   openSSL_MD5 md5Hasher;
+  openSSL_SHA256 sha256;
   userMenu menu(io, md5Hasher);
   std::string input = "The quick brown fox jumps over the lazy dog";
-  std::string hashedInput = md5Hasher.hashString(input);
+  std::string md5Hash = md5Hasher.hashString(input);
+  std::string shaHash = sha256.hashString(input);
 
-  std::cout << std::format("Original: {}\nHashed: {}", input, hashedInput) << std::endl;
+  std::cout << std::format("Original: {}\nMD5: {}\nSHA256: {}\n", input, md5Hash, shaHash) << std::endl;
   return 0;
 }
