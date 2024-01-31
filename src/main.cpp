33467bbbed767ab820c1e06732821a443a0c66e0
Cosmao <emil.kool@hotmail.com>
2024-01-31 17:39:34 +0100

    Cleaned main

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,13 @@
 #include "headers/io_file.h"
 #include "headers/openSSL_EVP.h"
 #include "headers/userMenu.h"
-#include <format>
 
-#define filePath_pw "../../common_pw.txt"
-#define filePath_hashed "../../hashes.txt"
 #define filePath_usr "../../users.txt"
-#define filePath_test "../../test.txt"
 
 int main(void) {
-  // io_file hashed_files(filePath_hashed);
-  // io_file common_pw(filePath_pw);
   io_file users_list(filePath_usr);
   EVP_Hash EVP_Hasher(MD5_Hash);
   userMenu menu(users_list, EVP_Hasher);
-  io_file test(filePath_test);
-  test.writeLine("Test1");
   menu.menu();
-  // std::string input = "The quick brown fox jumps over the lazy dog";
-  // std::string evpHash = EVP_Hasher.hashString(input);
-  // std::string evpHashTwo = EVP_Hasher.hashString(input);
-  EVP_Hasher.switchHashMethod(SHA256_Hash);
-  // std::string evpSwitchTest = EVP_Hasher.hashString(input);
-  // EVP_Hasher.switchHashMethod(MD5_Hash);
-  // std::string md5Again = EVP_Hasher.hashString(input);
-  //
-  // std::cout << std::format(
-  //                  "Original: {}\nEVP: {}\nEVP: {}\nSHASWAP: {}\nMD5: {}",
-  //                  input, evpHash, evpHashTwo, evpSwitchTest, md5Again)
-  //           << std::endl;
   return 0;
 }
