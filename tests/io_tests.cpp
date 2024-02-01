a633d8336cfc38c168882d5fa348703cadfcd7b6
Cosmao <emil.kool@hotmail.com>
2024-02-01 15:09:56 +0100

    Baisc Io_file tests

---
+++ tests/io_tests.cpp
@@ -0,0 +1,17 @@
+#include "headers/io_file.h"
+#include <catch2/catch_test_macros.hpp>
+
+#define filePath "text.txt"
+
+TEST_CASE("Create and read file") {
+  {
+    io_file test_file(filePath);
+    REQUIRE(test_file.hasLine() == false);
+    test_file.resetStreamPos();
+    REQUIRE(test_file.writeLine("Testing") == true);
+    test_file.resetStreamPos();
+    REQUIRE(test_file.hasLine() == true);
+    REQUIRE(test_file.readLine() == "Testing");
+  }
+  std::remove(filePath);
+}
